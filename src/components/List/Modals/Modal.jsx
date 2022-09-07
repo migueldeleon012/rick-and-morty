3ad0f2209567262f088d6efcc9b625473e657995
Miguel <miguelangelodeleonl12@gmail.com>
2022-09-07 11:54:56 +0200

    feat: made modal props

--- src/components/List/Modals/Modal.jsx
+++ src/components/List/Modals/Modal.jsx
@@ -3,7 +3,18 @@ import './modal.css';
 const Modal = (props) => {
   console.log(props);
 
-  return <div></div>;
+  return (
+    <div>
+      <img src={props.image} alt={`Image: ${props.name}`} />
+      <main>
+        <p>{props.name}</p>
+        <p>{props.species}</p>
+        <p>{props.gender}</p>
+        <p>{props.origin}</p>
+        <p>{props.location}</p>
+      </main>
+    </div>
+  );
 };
 
 export default Modal;
