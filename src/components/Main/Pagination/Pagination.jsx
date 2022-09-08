1c883c4eaa998490159e9334c51bfe16fd592471
Miguel <miguelangelodeleonl12@gmail.com>
2022-09-08 14:44:41 +0200

    fix: page number being string + number

--- src/components/Main/Pagination/Pagination.jsx
+++ src/components/Main/Pagination/Pagination.jsx
@@ -5,14 +5,14 @@ const Pagination = ({ currentPage, setCurrentPage, currentDataInfo }) => {
   const pageNumberArray = [];
   const numberPages = currentDataInfo.pages;
   const numberPagesDisplayed = 5;
-  const [maxPageLimit, setMaxPageLimit] = useState(4);
+  const [maxPageLimit, setMaxPageLimit] = useState(5);
   const [minPageLimit, setMinPageLimit] = useState(0);
 
   for (let i = 1; i <= numberPages; i++) {
     pageNumberArray.push(i);
   }
 
-  const changePages = (e) => setCurrentPage(e.target.textContent);
+  const changePages = (e) => setCurrentPage(Number(e.target.textContent));
 
   const handlePrevButton = () => {
     if ((currentPage - 1) % maxPageLimit === 0) {
@@ -23,6 +23,8 @@ const Pagination = ({ currentPage, setCurrentPage, currentDataInfo }) => {
   };
 
   const handleNextButton = () => {
+    console.log(currentPage);
+
     if (currentPage + 1 > maxPageLimit) {
       setMaxPageLimit(maxPageLimit + numberPagesDisplayed);
       setMinPageLimit(minPageLimit + numberPagesDisplayed);
@@ -33,7 +35,11 @@ const Pagination = ({ currentPage, setCurrentPage, currentDataInfo }) => {
   const renderPageNumber = (pageNumber) => {
     if (pageNumber <= maxPageLimit && pageNumber > minPageLimit) {
       return (
-        <button key={pageNumber} onClick={changePages}>
+        <button
+          key={pageNumber}
+          onClick={changePages}
+          className={pageNumber == currentPage ? 'active' : undefined}
+        >
           {pageNumber}
         </button>
       );
@@ -42,15 +48,29 @@ const Pagination = ({ currentPage, setCurrentPage, currentDataInfo }) => {
 
   return (
     <section className="pagination">
-      <button onClick={handlePrevButton}>prev</button>
+      <button onClick={handlePrevButton} disabled={currentPage <= 1}>
+        prev
+      </button>
       {minPageLimit >= 1 && (
-        <button onClick={handlePrevButton}>&hellip;</button>
+        <button onClick={handlePrevButton} disabled={currentPage <= 1}>
+          &hellip;
+        </button>
       )}
       {pageNumberArray.map(renderPageNumber)}
       {pageNumberArray.length > maxPageLimit && (
-        <button onClick={handleNextButton}>&hellip;</button>
+        <button
+          onClick={handleNextButton}
+          disabled={currentPage >= pageNumberArray.length}
+        >
+          &hellip;
+        </button>
       )}
-      <button onClick={handleNextButton}>next</button>
+      <button
+        onClick={handleNextButton}
+        disabled={currentPage >= pageNumberArray.length}
+      >
+        next
+      </button>
     </section>
   );
 };
