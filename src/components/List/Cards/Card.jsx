ee302eb4da38f34b2cd84dd1806ca5d3e56316fc
Miguel <miguelangelodeleonl12@gmail.com>
2022-09-08 09:06:37 +0200

    feat: added card and modal

--- src/components/List/Cards/Card.jsx
+++ src/components/List/Cards/Card.jsx
@@ -1,18 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Modal from '../Modals/Modal';
 
 const Card = (props) => {
-  const openModal = () => console.log('clicked');
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [scrollingDisabled, setScrollingDisabled] = useState(false);
+
+  const openModal = (open) => {
+    setIsModalOpen(open);
+    setScrollingDisabled(open);
+  };
+
+  //to prevent modal scrolling
+  useEffect(() => {
+    scrollingDisabled
+      ? (document.body.style.overflow = 'hidden')
+      : (document.body.style.overflow = 'auto');
+  }, [scrollingDisabled]);
+
+  const openModalHandler = () => openModal(true);
 
   return (
     <>
-      <figure onClick={openModal}>
-        <img src={props.image} alt={`Image: ${props.name}`} />
+      <figure onClick={openModalHandler}>
+        <img src={props.image} alt={`${props.name}`} />
         <figcaption>
           <p>{props.name}</p>
         </figcaption>
       </figure>
-      <Modal {...props} />
+      {isModalOpen && (
+        <Modal {...props} openModal={openModal} isModalOpen={isModalOpen} />
+      )}
     </>
   );
 };
--- src/components/List/Modals/Modal.jsx
+++ src/components/List/Modals/Modal.jsx
@@ -1,17 +1,18 @@
 import './modal.css';
 
 const Modal = (props) => {
-  console.log(props);
-
   return (
-    <div>
-      <img src={props.image} alt={`Image: ${props.name}`} />
-      <main>
-        <p>{props.name}</p>
-        <p>{props.species}</p>
-        <p>{props.gender}</p>
-        <p>{props.origin}</p>
-        <p>{props.location}</p>
+    <div className="modal" onClick={() => props.openModal(false)}>
+      <main className="modal__main">
+        <button className="modal__main--btn">X</button>
+        <img src={props.image} alt={`${props.name}`} />
+        <div className="modal__main--text">
+          <h2>{props.name}</h2>
+          <p>Species: {props.species}</p>
+          <p>Gender: {props.gender}</p>
+          <p>Origin: {props.origin}</p>
+          <p>Location: {props.location}</p>
+        </div>
       </main>
     </div>
   );
